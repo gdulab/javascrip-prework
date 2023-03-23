396871a2365e22e91b9b37006cd69205b1894edd
gdulab <gdula.bartlomiej@gmail.com>
2023-03-23 12:53:06 +0100

    ADD js variables

---
+++ js/script.js
@@ -0,0 +1,5 @@
+var computerMove, playerMove;
+playerMove = 'papier';
+computerMove = 'kamień';
+printMessage('Ruch gracza to ' + playerMove + '.');
+printMessage('Zagrałem ' + computerMove + '! Jeśli Twój ruch to papier, to wygrywasz!');
